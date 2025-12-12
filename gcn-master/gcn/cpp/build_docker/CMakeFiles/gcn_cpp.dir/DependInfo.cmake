b0e0a7ad31a0886743883ca4ed113295610684ec
wanyu32727 <3420979357@qq.com>
2025-12-12 15:49:25 +0800

    cpp modify 12.12

---
+++ gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/backward.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/backward.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/data_loader.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/data_loader.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/gcn_layer.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/gcn_layer.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/graph_utils.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/graph_utils.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/loss.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/loss.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/main.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/main.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/optimizer.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/optimizer.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/scheduler.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/scheduler.cpp.o"
+  "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/src/trainer.cpp" "/workspace/hlexperience/gcnex/gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/src/trainer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_METIS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../include"
+  "../../../../../ramulator-pim-master/zsim-ramulator/misc/hooks"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gcn-master/gcn/cpp/build_docker/CMakeFiles/gcn_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gcn_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/trainer.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/gcn_layer.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/scheduler.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/optimizer.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/loss.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/backward.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/graph_utils.cpp.o"
+  "CMakeFiles/gcn_cpp.dir/src/data_loader.cpp.o"
+  "gcn_cpp.pdb"
+  "gcn_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gcn_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
